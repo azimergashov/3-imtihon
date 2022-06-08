8a5bcae3f76b39393cf66847cf3b17652e323939
azimergashov <azimboy.ergashov.1@gmail.com>
2022-06-08 23:02:46 +0600

    finish 2-execise

--- js/masalalar.js
+++ js/masalalar.js
@@ -1,22 +1,95 @@
 //MASALALAR
 
 // 1-MASALA
+////--------------------------------------------------------------------------
 
-const aaaa = prompt("sonni kiriting")
+// const number = prompt("sonni kiriting!");
+// const numberTwo = prompt("2-sonni kiriting!");
+// const numberThree = prompt("3-soni kiriting!");
+
+// const arrr = [number, numberTwo, numberThree ];
+
+// const sss = numberTwo * 1
+
+// arrr.forEach(e =>{
+
+// })
 
 
-console.log(aaaa);
 
-
-
+////--------------------------------------------------------------------------
 
 // 2-MASALA
+////--------------------------------------------------------------------------
 
+
+// //1
+// let array = ["Jazz", "Blues"]
+
+// console.log(array);
+// //2
+// array.push("Rock-n-roll")
+
+// console.log(array);
+// //3
+// array[1] = "Classic"
+
+// console.log(array);
+// //4
+// array.shift()
+
+// console.log(array);
+
+// //5
+// array.unshift("Reggae")
+// array.unshift("Rap")
+
+// console.log(array);
+
+
+////--------------------------------------------------------------------------
 
 // 3-MASALA
+////--------------------------------------------------------------------------
 
 
+// let salaries = {
+// 	"Akmal": 1200,
+// 	"Salim": 5200,
+// 	"Karima": 1800
+// }
+// const  sum = []
+
+// for(let item of salaries){
+
+//     sum.push(item)
+// }
+
+// console.log(sum);
+
+
+////--------------------------------------------------------------------------
 // 4-MASALA
+////--------------------------------------------------------------------------
+
+ const sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+ const toqSon = []
+ const juftSon = []
+
+ for(let s of sonlar){
+
+    if( s % 2 == 0){
+        juftSon.push(s)
+    }else{
+        toqSon.push(s)
+    }
+
+    function calcSum()
+ }
+
+ console.log(toqSon);
 
 
+////--------------------------------------------------------------------------
 // 5-MASALA
